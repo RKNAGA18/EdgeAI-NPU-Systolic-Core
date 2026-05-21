716028b8d52c3869a2a5d132d52ffacedcfe33b2
R NAGA ARJUN <rknarjun396@gmail.com>
2026-05-21 18:17:41 +0530

    Update systolic_array_8x8.sv

--- src/systolic_array_8x8.sv
+++ src/systolic_array_8x8.sv
@@ -2,19 +2,19 @@ module systolic_array_8x8 (
     input  logic               clk,
     input  logic               rst_n,
     input  logic               weight_load_en,
-    input  logic [63:0]        act_in_left_flat, 
-    input  logic [255:0]       psum_in_top_flat, 
-    output logic [255:0]       psum_out_bottom_flat 
+    input  logic [31:0]        act_in_left_flat,   // 8 rows * 4 bits
+    input  logic [127:0]       psum_in_top_flat,   // 8 cols * 16 bits
+    output logic [127:0]       psum_out_bottom_flat 
 );
-    logic signed [7:0]  act_wires  [0:7][0:8]; 
-    logic signed [31:0] psum_wires [0:8][0:7];
+    logic signed [3:0]  act_wires  [0:7][0:8]; 
+    logic signed [15:0] psum_wires [0:8][0:7];
 
     genvar i;
     generate
         for (i = 0; i < 8; i++) begin : edge_wiring
-            assign act_wires[i][0]    = act_in_left_flat[(i*8)+7 : i*8];
-            assign psum_wires[0][i]   = psum_in_top_flat[(i*32)+31 : i*32];
-            assign psum_out_bottom_flat[(i*32)+31 : i*32] = psum_wires[8][i];
+            assign act_wires[i][0]    = act_in_left_flat[(i*4)+3 : i*4];
+            assign psum_wires[0][i]   = psum_in_top_flat[(i*16)+15 : i*16];
+            assign psum_out_bottom_flat[(i*16)+15 : i*16] = psum_wires[8][i];
         end
     endgenerate
 
